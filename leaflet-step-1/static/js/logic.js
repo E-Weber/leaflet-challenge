2c936380e0a98b719e8391786a5e09d4493c505f
E-Weber <ewebersms@gmail.com>
2021-05-17 19:52:22 -0500

    createMap

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -2,19 +2,31 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 console.log(queryUrl);
 
-// create a map object
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
+// get request to query URL
+d3.json(queryUrl).then(function (data) {
+    createFeatures(data.features);
 });
 
-// add a tile layer
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-}).addTo(myMap);
+function createFeatures(earthquakeData) {
+
+    // give each feature a popup
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    function pointFunction(feature, layer) {
+        return L.circleMarker(layer, { radius: feature.properties.mag * 10 });
+    }
+    var earthquakes = L.geoJSON(earthquakeData, {
+        // add popups
+        onEachFeature: onEachFeature,
+        // add circles
+        pointToLayer: pointFunction
+    });
+
+    // send our earthquake to createMap function
+    createMap(earthquakes);
+}
+
 
